327b7215a3b6050826826797574e5a8a70e2f8d7
art-tal <talanov-artem@ukr.net>
2021-10-12 16:45:54 +0300

    2021.10.12

---
+++ src/components/app/Navbar.vue
@@ -0,0 +1,70 @@
+<template>
+  <nav class="navbar orange lighten-1">
+    <div class="nav-wrapper">
+      <div class="navbar-left">
+        <a
+          href="#"
+          @click.prevent="$emit('click')"
+        >
+          <i class="material-icons black-text">dehaze</i>
+        </a>
+        <span class="black-text">12.12.12</span>
+      </div>
+
+      <ul class="right hide-on-small-and-down">
+        <li>
+          <a
+            ref="dropdown"
+            class="dropdown-trigger black-text"
+            href="#"
+            data-target="dropdown"
+          >
+            USER NAME
+            <i class="material-icons right">arrow_drop_down</i>
+          </a>
+
+          <ul
+            id="dropdown"
+            class="dropdown-content"
+          >
+            <li>
+              <a
+                href="#"
+                class="black-text"
+              >
+                <i class="material-icons">account_circle</i>Профиль
+              </a>
+            </li>
+            <li
+              class="divider"
+              tabindex="-1"
+            />
+            <li>
+              <a
+                href="#"
+                class="black-text"
+              >
+                <i class="material-icons">assignment_return</i>Выйти
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  mounted () {
+    M.Dropdown.init(this.$refs.dropdown, {
+      constrainWidth: false
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,62 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    meta: { layout: 'main' },
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'empty' },
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: { layout: 'empty' },
+    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: { layout: 'main' },
+    component: () => import(/* webpackChunkName: "about" */ '../views/Categories.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: { layout: 'main' },
+    component: () => import(/* webpackChunkName: "about" */ '../views/Record.vue')
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: { layout: 'main' },
+    component: () => import(/* webpackChunkName: "about" */ '../views/DetailRecord.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: { layout: 'main' },
+    component: () => import(/* webpackChunkName: "about" */ '../views/History.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main' },
+    component: () => import(/* webpackChunkName: "about" */ '../views/Profile.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: { layout: 'main' },
+    component: () => import(/* webpackChunkName: "about" */ '../views/Planning.vue')
   }
 ]
 
